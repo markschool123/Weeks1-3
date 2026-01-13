4bbfb18c4f824353d3800197d08ba7be1fca6d7c
Mark Fernandes <ferna100@shernet.sheridancollege.ca>
2026-01-13 12:57:38 -0500

    added curve to linear interpolation

--- Assets/Scripts/LinearInterpolation.cs
+++ Assets/Scripts/LinearInterpolation.cs
@@ -6,6 +6,8 @@ public class LinearInterpolation : MonoBehaviour
     public Transform end;
     public float t = 0;
 
+    public AnimationCurve curve;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,6 +24,6 @@ public class LinearInterpolation : MonoBehaviour
             t = 0;
         }
 
-        transform.position = Vector2.Lerp(start.position, end.position, t);
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
     }
 }
