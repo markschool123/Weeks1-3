c9df86ebec2e86cf997bbd370e49eff17988553b
Mark Fernandes <ferna100@shernet.sheridancollege.ca>
2026-01-20 10:47:16 -0500

    used an array and a list to spawn

--- Assets/Scripts/SpriteChanges.cs
+++ Assets/Scripts/SpriteChanges.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,19 +7,26 @@ public class SpriteChanges : MonoBehaviour
 {
     public Color col; 
     public SpriteRenderer spriteRenderer;
+    public List<Sprite> barrels;
+    
+    public int randomNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PickARandomSpirite();
     }
 
     // Update is called once per frame
     void Update()
     {
-      //if (Keyboard.current.anyKey.wasPressedThisFrame == true)
-      //{
-          //PickARandomColour();
-      //}
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            //PickARandomColour();
+            if (barrels.Count > 0)
+            { 
+            PickARandomSpirite();
+        }
+    }
 
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
@@ -29,12 +38,27 @@ public class SpriteChanges : MonoBehaviour
         {
             spriteRenderer.color = Color.white;
         }
+
+       if (Mouse.current.leftButton.wasPressedThisFrame == true && barrels.Count > 0);
+        {
+            barrels.RemoveAt(0);
+        }
     }
 
     void PickARandomColour()
     {
         spriteRenderer.color = Random.ColorHSV(); 
     }
-}
+
+    void PickARandomSpirite()
+
+
+    {
+
+        randomNumber = Random.Range(0, barrels.Count);
+
+         spriteRenderer.sprite = barrels[randomNumber];
+        }
+    }
 
 
