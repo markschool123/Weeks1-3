f24561b693e67b0e0e5256c7cae968f939738c2c
Mark Fernandes <ferna100@shernet.sheridancollege.ca>
2026-01-26 13:33:02 -0500

    added more comments

--- Assets/Scripts/Assignment/Pulse.cs
+++ Assets/Scripts/Assignment/Pulse.cs
@@ -14,14 +14,18 @@ public class Pulse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // increase t by the time since last frame
         t += Time.deltaTime;
 
+        // reset t back to 0, creating a loop
         if (t > 1)
         {
             t = 0;
 
         }
+        //get curve value at time t
         y = curve.Evaluate(t);
+        // set the scale based opn the curve value, making the object grow/shrink over time
         transform.localScale = Vector2.one * curve.Evaluate(t);
         
     }
